db0c530941fa8eaa90eda9350294e3f238f41acf
Sebastian Sudra <Sebastian.Sudra@pentacomp.pl>
2021-08-12 00:32:35 +0200

    Serwer + klient

---
+++ GrpcService/GrpcClient/Program.cs
@@ -0,0 +1,21 @@
+﻿using Grpc.Net.Client;
+using GrpcService;
+using System;
+using static GrpcService.Greeter;
+
+namespace GrpcClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using var channel = GrpcChannel.ForAddress("http://localhost:5000");
+            var client = new GreeterClient(channel);
+            var reply = client.SayHelloAsync(
+                              new HelloRequest { FirstName = "Sebastian", LastName = "Enigman" });
+            Console.WriteLine("Greeting: " + reply.ResponseAsync.Result.Message);
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
